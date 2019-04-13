105cb504e84e3a0960f1e5b4e4679face01ea959
sreekanthvaripalli <49576806+sreekanthvaripalli@users.noreply.github.com>
2019-04-13 10:39:21 +0800

    updated values
     removed hardcoded values for the keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe={
-:publishable-key => 'pk+live_123123123123',
-:secret_key => 'sk_live_0aASXgdt5hnkiudbc5tgse65'
+:publishable_key => ENV['PUBLISHABLE_KEY'],
+:secret_key => ENV['SECRET_KEY']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
